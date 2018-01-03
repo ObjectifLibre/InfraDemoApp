1299f69b1a5b917ddcf287a1b68497d091d2992f
Gauvain Pocentek <gauvain.pocentek@objectif-libre.com>
2018-01-03 10:37:14 +0100

    v1.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$version = "1.0";
+$hostname = gethostname();
+
+?>
+<html>
+<body>
+<h1>DemoApp v.<?php echo $version; ?></h1>
+<h2>Running on <?php echo $hostname; ?></h2>
+</body>
+</html>
